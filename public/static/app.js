d156d4ad2e33d4d274246fca65c7b4d9b95fe1b3
Maelsh Pro <maelshspro@gmail.com>
2025-12-09 03:33:28 +0200

    update

--- public/static/app.js
+++ public/static/app.js
@@ -449,7 +449,7 @@ async function handleLogin(e) {
       showToast(window.lang === 'ar' ? 'مرحباً بك!' : 'Welcome!', 'success');
 
       // Reload to refresh data
-      //setTimeout(() => window.location.reload(), 500);
+      setTimeout(() => window.location.reload(), 50);
     } else {
       showAuthMessage(data.error || 'Login failed', 'error');
     }
