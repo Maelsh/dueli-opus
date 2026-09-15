51ae142e263f4004c5137403749146bd10a252b8
Maelsh Pro <maelshspro@gmail.com>
2026-09-15 21:02:21 +0300

    chore(b10): drop temp i18n helper scripts

--- dev-tools/b10-step.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let s = fs.readFileSync('src/i18n/ar.ts', 'utf8');
-s = s.replace('SELF-AR', 'PLACEHOLDER-A');
-fs.writeFileSync('src/i18n/ar.ts', s, 'utf8');
-console.log('step ok ' + s.includes('rating_self_forbidden'));
